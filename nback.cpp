b92cb64b9e8c28b8857ea5cac608e9315790d6f5
jelly <jelnonymous@gmail.com>
2018-10-07 12:47:56 -0700

    replacing source with latest from hdd

--- nback.cpp
+++ nback.cpp
@@ -1,24 +1,14 @@
 
-#define STL_INCLUDE 0
 
 #include <cassert>
+#include <cerrno>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <unistd.h>
-
-#ifdef STL_INCLUDE
-#include <algorithm>
-#include <chrono>
-#include <future>
-#include <iostream>
-#include <mutex>
-#include <string>
-#include <thread>
-#else // STL_INCLUDE
-#include <cerrno>
 #include <ctime>
-#endif // STL_INCLUDE
+#include <getopt.h>
+#include <new>
+#include <unistd.h>
 
 template<typename t_type, int size>
 class ring_t {
@@ -193,12 +183,14 @@ void shuffle_ints(int *array, size_t n) {
 		size_t i;
 		for (i = 0; i < n - 1; i++) {
 			size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+			assert(j < n);
 			int t = array[j];
 			array[j] = array[i];
 			array[i] = t;
 		}
 	}
 }
+
 // Nback logic
 
 typedef ring_t<int, 7> n_back_buffer;
@@ -225,6 +217,26 @@ bool is_guess_correct(const n_back_buffer &past, int guess_back) {
 	return result;
 }
 
+bool has_nback(const n_back_buffer &past) {
+	bool result= false;
+
+	int counter= 0;
+	int head_value= -1;
+
+	for (n_back_buffer::c_const_reverse_iterator it= past.iterate_reverse();
+		!result && it.is_valid();
+		counter++, it.next()) {
+
+		if (counter==0) {
+			head_value= it.get();
+		} else if (it.get()==head_value) {
+			result= true;
+		}
+	}
+
+	return result;
+}
+
 // User interface
 
 const int msec_per_sec= 1000;
@@ -233,7 +245,58 @@ const int usec_per_sec= usec_per_msec * msec_per_sec;
 
 class i_nback_value_provider {
 public:
-	virtual int get_next_value() const= 0;
+	virtual bool has_next() const= 0;
+	virtual int get_next_value()= 0;
+};
+
+class generic_value_provider : public i_nback_value_provider {
+private:
+	enum {
+		max_impl_size= 1024
+	};
+public:
+
+	virtual bool has_next() const {
+		return get()->has_next();
+	}
+
+	virtual int get_next_value() {
+		return get()->get_next_value();
+	}
+	
+	inline void *get_allocated_storage() {
+		return raw;
+	}
+
+private:
+	inline i_nback_value_provider *get() {
+		return reinterpret_cast<i_nback_value_provider*>(raw);
+	}
+	inline const i_nback_value_provider *get() const {
+		return reinterpret_cast<const i_nback_value_provider*>(raw);
+	}
+
+	unsigned char raw[max_impl_size];
+};
+
+class value_provider_factory {
+public:
+
+	template <typename t_derived>
+	i_nback_value_provider *create() {
+		static_assert(
+			sizeof(t_derived) <= sizeof(storage),
+			"derived type cannot be larger than max!");
+
+		if (sizeof(t_derived) <= sizeof(storage)) {
+			return new(storage.get_allocated_storage()) t_derived;
+		} else {
+			return 0;
+		}
+	}
+
+private:
+	generic_value_provider storage;
 };
 
 class card_value_provider : public i_nback_value_provider {
@@ -244,20 +307,71 @@ private:
 		card_count= max_value*suite_count
 	};
 public:
-	card_value_provider() {
+	card_value_provider() : m_index(0) {
+		// Assign card values
 		for (int suite_inc= 0; suite_inc < suite_count; ++suite_inc) {
-			for (int value_seed= 1; value_seed <= max_value; ++value_seed) {
-				m_cards[suite_inc*suite_count + value_seed-1]= value_seed;
+			for (int value_inc= 0; value_inc < max_value; ++value_inc) {
+				m_cards[suite_inc*max_value + value_inc]= value_inc + 1;
 			}
 		}
+		
+		// go ahead and shuffle
+		shuffle_ints(&m_cards[0], card_count);
+		shuffle_ints(&m_cards[0], card_count);
+		shuffle_ints(&m_cards[0], card_count);
 	}
 
-	virtual int get_next_value() const {
-		
+	virtual bool has_next() const {
+		return m_index < card_count;
+	}
+
+	virtual int get_next_value() {
+		assert(m_index < card_count && m_index >= 0);
+		return m_cards[m_index++];
 	}
 
 private:
 	int m_cards[card_count];
+	int m_index;
+};
+
+class random_value_provider : public i_nback_value_provider {
+public:
+	virtual bool has_next() const {
+		return true;
+	}
+
+	virtual int get_next_value() {
+		return rand() % 10 + 1;
+	}
+};
+
+class test_value_provider : public i_nback_value_provider {
+public:
+	virtual bool has_next() const;
+
+	virtual int get_next_value() {
+		return testbuff[test_index++];
+	}
+private:
+	int test_index= 0;
+	static int testbuff[];
+};
+
+int test_value_provider::testbuff[]= { 5, 6, 7, 8, 9, 4, 5, 3 };
+
+bool test_value_provider::has_next() const {
+	return test_index < ARRAY_SIZE(testbuff);
+}
+
+//#define TEST_VALUE_PROVIDER_FACTORY_ASSERT
+class test_static_assert_value_provider : public i_nback_value_provider {
+public:
+	virtual bool has_next() const { return false; }
+	virtual int get_next_value() { return 0; }
+private:
+	// test: trigger size assert
+	int mega_buff[4096];
 };
 
 void print_n_back_buffer(const n_back_buffer &buffer) {
@@ -277,6 +391,7 @@ void print_current_value_line(int current_value, bool ping) {
 	fflush(stdout);
 }
 
+//todo: dont allow 'enter' to show history
 bool try_get_guess_with_timeout(
 	int current_value, 
 	const optional<int> &opt_guess_timeout_sec,
@@ -291,7 +406,7 @@ bool try_get_guess_with_timeout(
 	const int per_select_timeout_usec= 50 * usec_per_msec;
 	const int time_to_show_ping_usec= 150 * usec_per_msec;
 	const int iterations_to_show_ping= time_to_show_ping_usec/per_select_timeout_usec;
-	const int default_guess_timeout_sec= 3;
+	const int default_guess_timeout_sec= 2;
 
 	const int total_timeout_seconds= opt_guess_timeout_sec.is_set
 		? opt_guess_timeout_sec.value
@@ -308,7 +423,9 @@ bool try_get_guess_with_timeout(
 		FD_ZERO(&read_fds);
 		FD_SET(STDIN_FILENO, &read_fds);
 
-		print_current_value_line(current_value, cnt < iterations_to_show_ping);
+		if (cnt==0 || cnt==iterations_to_show_ping) {
+			print_current_value_line(current_value, cnt < iterations_to_show_ping);
+		}
 
 		select_result= select(1, &read_fds, NULL, NULL, &tv);
 		if (select_result == -1 && errno != EINTR) {
@@ -334,60 +451,136 @@ bool try_get_guess_with_timeout(
 	return result;
 }
 
-#ifdef STL_INCLUDE
-// shameless copy from https://stackoverflow.com/questions/865668/how-to-parse-command-line-arguments-in-c
+struct nback_options {
+	// modes
+	int test_mode;
+	int random_mode;
+	// settings
+	optional<int> timeout_sec;
+	int print_buffer_on_guess;
+	int clear_buffer_on_guess;
 
-using namespace std;
-
-char* get_cmd_option(char **begin, char **end, const std::string &option) {
-	char **itr = std::find(begin, end, option);
-	if (itr != end && ++itr != end) {
-		return *itr;
+	void clear() {
+		test_mode= 0;
+		random_mode= 0;
+		timeout_sec= {false, 0};
+		print_buffer_on_guess= 1;
+		clear_buffer_on_guess= 0;
 	}
-	return 0;
+};
+
+void display_usage() {
+	puts("N-Back brain improvement console application. by: Jelley   ");
+	puts("Options:                                                   ");
+	puts("  --test           : test mode, short and predictable      ");
+	puts("  --random         : puts the game in 'true random' mode   ");
+	puts("  --hide_history   : disables history display on each guess");
+	puts("  --guess_clear    : clears history on each guess (resets) ");
+	puts("  --seconds [v]    : set timeout for each guess (from 2)   ");
+	puts("  --help, -h, -?   : display this message                  ");
 }
 
-bool cmd_option_exists(char **begin, char **end, const std::string &option) {
-	return std::find(begin, end, option) != end;
+bool get_options(int argc, char *argv[], nback_options &out_options) {
+	bool success= true;
+	int getopt_code;
+	const char *const opt_string= "s:h?";
+	const struct option long_options[]= {
+		{ "test",         no_argument, &out_options.test_mode, 1 },
+		{ "random",       no_argument, &out_options.random_mode, 1 },
+		{ "hide_history", no_argument, &out_options.print_buffer_on_guess, 0 },
+		{ "guess_clear",  no_argument, &out_options.clear_buffer_on_guess, 1 },
+		{ "seconds",      required_argument, 0, 's' },
+		{ "help",         no_argument, 0, 'h' },
+		{ 0, 0, 0, 0}
+	};
+
+	out_options.clear();
+
+	do {
+		// getopt_long stores the option index here
+		int option_index= 0;
+
+		getopt_code= getopt_long(argc, argv, opt_string, long_options, &option_index);
+
+		switch (getopt_code) {
+			case -1:
+				break;
+
+			case 0:
+				// If this option set a flag, do nothing else now.
+				if (long_options[option_index].flag != 0) {
+					break;
+				}
+				//TODO: What is this case?
+				break;
+
+			case 's':
+				if (sscanf(optarg, "%d", &out_options.timeout_sec.value) != 0) {
+					out_options.timeout_sec.is_set= true;
+				} else {
+					puts("Option '-s' requires an integer value.");
+					success= false;
+				}
+				break;
+
+			case 'h':
+			case '?':
+				success= false;
+				break;
+
+			default:
+				abort ();
+		}
+	} while (getopt_code != -1);
+
+	return success;
 }
-
-optional<int> try_get_cmd_option_int(char **begin, char **end, const std::string &option) {
-	optional<int> result= { false, 0 };
-	const char *const cmd_string= get_cmd_option(begin, end, option);
-
-	if (cmd_string != 0 && sscanf(cmd_string, "%d", &result.value) > 0) {
-		result.is_set= true;
-	}
-
-	return result;
-}
-#endif // STL_INCLUDE
 
 // Entry point
+value_provider_factory factory;
+
+struct nback_results {
+	int correct;
+	int incorrect;
+	int misses;
+};
 
 int main(int argc, char *argv[]) {
 
+	nback_results res= {0};
 	n_back_buffer past;
-	int test_index= 0;
-	const int testbuff[]= { 5, 6, 7, 8, 9, 4, 5, 3 };
+	i_nback_value_provider *prov= 0;
 
+	// Setup
 	run_unit_tests_ring_t();
-
-#ifdef STL_INCLUDE
-	bool test_mode_enabled= cmd_option_exists(argv, argv+argc, "--test");
-	optional<int> timeout_sec= try_get_cmd_option_int(argv, argv+argc, "--seconds");
-	//TODO optional mode for 'card selection' - randomness is limited to remaining cards in 40c deck
-#else // STL_INCLUDE
-	bool test_mode_enabled= false;
-	optional<int> timeout_sec= { false, 0 };
-#endif // STL_INCLUDE
-
 	srand(time(0));
+	
+	// Options
+	nback_options options;
+	if (!get_options(argc, argv, options)) {
+		display_usage();
+		return 1;
+	}
 
-	while (!test_mode_enabled || test_index < ARRAY_SIZE(testbuff)) {
+#ifdef TEST_VALUE_PROVIDER_FACTORY_ASSERT
+	if (true) {
+		prov= factory.create<test_static_assert_value_provider>();
+	} else
+#endif // TEST_VALUE_PROVIDER_FACTORY_ASSERT
+	if (options.test_mode) {
+		prov= factory.create<test_value_provider>();
+	} else if (options.random_mode) {
+		prov= factory.create<random_value_provider>();
+	} else {
+		prov= factory.create<card_value_provider>();
+	}
 
-		int current_value= test_mode_enabled ? testbuff[test_index] : rand() % 10 + 1;
+	puts("Ready yourself...");
+	sleep(1);
+
+	while (prov->has_next()) {
 		int guess_back;
+		int current_value= prov->get_next_value();
 
 		if (past.is_full()) {
 			past.dequeue();
@@ -395,25 +588,34 @@ int main(int argc, char *argv[]) {
 
 		past.enqueue(current_value);
 
-		if (try_get_guess_with_timeout(current_value, timeout_sec, guess_back)) {
-			print_n_back_buffer(past);
-
-			if (is_guess_correct(past, guess_back)) {
-				printf("correct! starting over.\n");
-			} else {
-				printf("wrong! starting over.\n");
+		if (try_get_guess_with_timeout(current_value, options.timeout_sec, guess_back)) {
+			if (options.print_buffer_on_guess) {
+				print_n_back_buffer(past);
 			}
 
-			past.clear();
-			sleep(2);
-		}
+			if (is_guess_correct(past, guess_back)) {
+				puts("correct! resuming...");
+				res.correct++;
+			} else {
+				puts("wrong! resuming...");
+				res.incorrect++;
+			}
 
-		if (test_mode_enabled) {
-			test_index++;
+			if (options.clear_buffer_on_guess) {
+				past.clear();
+			}
+			sleep(2);
+		} else {
+			// todo: should misses count all nbacks, regardless of whether past is cleared?
+			res.misses+= has_nback(past) ? 1 : 0;
 		}
 	}
 
-	putc('\n', stdout);
+	puts("... That's all!");
+	
+	printf("correct: %d\n", res.correct);
+	printf("incorrect: %d\n", res.incorrect);
+	printf("missed: %d\n", res.misses);
 
 	return 0;
 }
